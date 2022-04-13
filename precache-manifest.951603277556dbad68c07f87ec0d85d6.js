d18e77f3c4412ae01b6ee6656eb466120535b277
NicMilli <91165462+NicMilli@users.noreply.github.com>
2022-04-13 18:35:08 -0400

    Updates

---
+++ precache-manifest.951603277556dbad68c07f87ec0d85d6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ba0a2df7b01e8eebafde6ab034b5126",
+    "url": "/SupplyChain/index.html"
+  },
+  {
+    "revision": "03210a6ba9c2e2c6538a",
+    "url": "/SupplyChain/static/css/2.2e827cdd.chunk.css"
+  },
+  {
+    "revision": "ee6de5f9e3008f0cba20",
+    "url": "/SupplyChain/static/css/main.006945ec.chunk.css"
+  },
+  {
+    "revision": "03210a6ba9c2e2c6538a",
+    "url": "/SupplyChain/static/js/2.a7eaff1d.chunk.js"
+  },
+  {
+    "revision": "ee6de5f9e3008f0cba20",
+    "url": "/SupplyChain/static/js/main.020a1cb5.chunk.js"
+  },
+  {
+    "revision": "9480ecd8dd23446e7b3b",
+    "url": "/SupplyChain/static/js/runtime-main.de6d2efd.js"
+  },
+  {
+    "revision": "95b727084ce9c269fa56d19f6a1b5358",
+    "url": "/SupplyChain/static/media/extension1.95b72708.png"
+  },
+  {
+    "revision": "4f31e465ddc6a31d8c38fe9efa8fb662",
+    "url": "/SupplyChain/static/media/extension2.4f31e465.png"
+  },
+  {
+    "revision": "b8d3a56101966b2394dfad32bbb3d5df",
+    "url": "/SupplyChain/static/media/mylogo.b8d3a561.png"
+  }
+]);
