6586005e56cfa45bb97b464a3cd8e76cebd9748f
NicMilli <91165462+NicMilli@users.noreply.github.com>
2022-04-19 15:22:17 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/SupplyChain/precache-manifest.b19ba3f46452ee0881d4d1f33fac1a25.js"
+  "/SupplyChain/precache-manifest.e29910a03ab42d7e600f005bce41f72c.js"
 );
 
 self.addEventListener('message', (event) => {
