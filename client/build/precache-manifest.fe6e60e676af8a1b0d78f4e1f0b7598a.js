42fbafcca179010d4bc6e57fc885cf4ee9ff4110
NicMilli <91165462+NicMilli@users.noreply.github.com>
2023-01-26 12:33:05 -0800

    Goerli test net deployment

---
+++ client/build/precache-manifest.fe6e60e676af8a1b0d78f4e1f0b7598a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2845ffc7e66228f80a3672237d5a546",
+    "url": "/SupplyChain/index.html"
+  },
+  {
+    "revision": "aa3fcb4108ea2db28fe1",
+    "url": "/SupplyChain/static/css/2.2e827cdd.chunk.css"
+  },
+  {
+    "revision": "4ceab44ad8b49988c944",
+    "url": "/SupplyChain/static/css/main.3d7b80c0.chunk.css"
+  },
+  {
+    "revision": "aa3fcb4108ea2db28fe1",
+    "url": "/SupplyChain/static/js/2.678c62c9.chunk.js"
+  },
+  {
+    "revision": "4ceab44ad8b49988c944",
+    "url": "/SupplyChain/static/js/main.61d23beb.chunk.js"
+  },
+  {
+    "revision": "9480ecd8dd23446e7b3b",
+    "url": "/SupplyChain/static/js/runtime-main.de6d2efd.js"
+  },
+  {
+    "revision": "95b727084ce9c269fa56d19f6a1b5358",
+    "url": "/SupplyChain/static/media/extension1.95b72708.png"
+  },
+  {
+    "revision": "4f31e465ddc6a31d8c38fe9efa8fb662",
+    "url": "/SupplyChain/static/media/extension2.4f31e465.png"
+  },
+  {
+    "revision": "b8d3a56101966b2394dfad32bbb3d5df",
+    "url": "/SupplyChain/static/media/mylogo.b8d3a561.png"
+  }
+]);
